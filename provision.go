6f16a76640498c09e234f2482c7076c0ea0c0971
ken5scal <kengoscal@gmail.com>
2017-07-18 20:15:02 +0900

    Add batch user method

--- provision.go
+++ provision.go
@@ -48,15 +48,14 @@ type User struct {
 	Groups                 []string `json:"groups,omitempty"`
 }
 
-type UserInfo struct {
-	Users  map[string]User     `json:"Users,omitempty"`
-	Groups map[string][]string `json:"Groups,omitempty"`
+type Status struct {
+	S string `json:"status"`
 }
 
 const (
-	CompanyID        = "8771312"
-	ProvisioningHash = "359fdfbc93bc6b8f1963c84e9db3539a5f3d688f394bd536e1ca6b77f8d5f101"
-	EndPointURL      = "https://lastpass.com/enterpriseapi.php"
+	CompanyID   = "8771312"
+	Secret      = "359fdfbc93bc6b8f1963c84e9db3539a5f3d688f394bd536e1ca6b77f8d5f101"
+	EndPointURL = "https://lastpass.com/enterpriseapi.php"
 )
 
 func main() {
@@ -68,14 +67,31 @@ func main() {
 		return
 	}
 
+	// Get an User
 	res, err := c.GetUserData("suzuki.kengo@moneyforward.co.jp")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	var user UserInfo
+	var user struct {
+		Users  map[string]User     `json:"Users,omitempty"`
+		Groups map[string][]string `json:"Groups,omitempty"`
+	}
 	decodeBody(res, &user)
+	fmt.Println(user)
+
+	// Add Users
+	var users []User
+	users = append(users, User{UserName: "kengoscal@gmail.com"})
+	res, err = c.BatchAddUsers(users)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var hoge Status
+	decodeBody(res, &hoge)
+	fmt.Println(hoge.S)
 }
 
 func NewClient(urlString string, logger *log.Logger) (*Client, error) {
@@ -93,7 +109,7 @@ func NewClient(urlString string, logger *log.Logger) (*Client, error) {
 		URL:              parsedURL,
 		HttpClient:       http.DefaultClient,
 		CompanyId:        CompanyID,
-		ProvisioningHash: ProvisioningHash,
+		ProvisioningHash: Secret,
 		Logger:           logger,
 	}, err
 }
@@ -112,6 +128,68 @@ func (c *Client) newRequest(ctx context.Context, method, spath string, body io.R
 	req.Header.Set("Content-Type", "application/json")
 	//req.Header.Set("User-Agent", userAgent)
 	return req, nil
+}
+
+/*
+  {
+    "cid": "8771312",
+    "provhash": "<Your API Secret>",
+    "cmd": "batchadd",
+    "data": [
+        {
+            "username": "user0@lastpass.com"
+        },
+        {
+            "username": "user1@lastpass.com",
+            "fullname": "John Doe"
+        },
+        {
+            "username": "user2@lastpass.com",
+            "fullname": "Mark Hunter",
+            "groups": [
+                "Group1",
+                "Group2",
+                "Group3"
+            ],
+            "attribs": {
+                "objectGUID": "d3b07384d113edec49eaa6238ad5ff00",
+                "Department": "Finance",
+                "EmployeeNumber": "192832"
+            }
+        },
+        {
+            "username": "user3@lastpass.com",
+            "fullname": "John Smith",
+            "password": "DefaultPassword"
+        },
+        {
+            "username": "user4@lastpass.com",
+            "fullname": "Jane Smith",
+            "password": "DefaultPassword",
+            "password_reset_required": false
+        }
+    ]
+}
+*/
+func (c *Client) BatchAddUsers(users []User) (*http.Response, error) {
+	body, err := json.Marshal(Request{
+		CompanyID:        c.CompanyId,
+		ProvisioningHash: c.ProvisioningHash,
+		Command:          "batchadd",
+		Data:             users,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(string(body))
+
+	res, err := http.Post(EndPointURL, "application/json; charset=utf-8", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 /*
