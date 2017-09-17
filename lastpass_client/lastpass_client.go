5baf5bba49f1f42d8210f5f319bd58c90374695f
ken5scal <kengoscal@gmail.com>
2017-09-17 17:23:07 +0900

    Merged two differnt clients

--- lastpass_client/lastpass_client.go
+++ lastpass_client/lastpass_client.go
@@ -6,13 +6,42 @@ import (
 	"errors"
 	"lastpass_provisioning/logger"
 	"net/http"
-	"lastpass_provisioning/client"
+	"net/url"
+	"log"
+	lp "lastpass_provisioning"
+	"net/http/httputil"
+	"fmt"
 )
 
 // LastPassClient is a Client that
 type LastPassClient struct {
-	Client    *client.Client
+	URL       *url.URL
+	APIKey    string
+	Verbose   bool
+	UserAgent string
+	Logger    *log.Logger
+	Headers   http.Header
 	CompanyID string
+}
+
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+// NewClient returns a general Client structure.
+func NewClient(apiKey string, endpointURL string, verbose bool) (*LastPassClient, error) {
+	parsedURL, err := url.ParseRequestURI(endpointURL)
+	if err != nil {
+		return nil, err
+	}
+	return &LastPassClient{
+		URL:       parsedURL,
+		APIKey:    apiKey,
+		Verbose:   verbose,
+		UserAgent: lastpass_config.DefaultUserAgent,
+		Headers:   http.Header{},
+		Logger:    nil,
+	}, nil
 }
 
 // NewLastPassClient returns LastPass Client from confFile
@@ -37,31 +66,74 @@ func NewLastPassClient(configFilePath string) *LastPassClient {
 		endPointURL = lastpass_config.DefaultBaseURL
 	}
 
-	client, err := client.NewClient(apiKey, endPointURL, os.Getenv("DEBUG") != "")
+	client, err := NewClient(apiKey, endPointURL, os.Getenv("DEBUG") != "")
 	logger.DieIf(err)
 
-	return &LastPassClient{
-		Client:    client,
-		CompanyID: companyID,
-	}
+	client.CompanyID = companyID
+	return client
 }
 
 // DoRequest executes LastPass specific request in JSON format and returns http Response
-func (c *LastPassClient) DoRequest(command string, data interface{}) (*http.Response, error) {
-	body := struct {
+func (c *LastPassClient) DoRequest(command string, payload interface{}) (*http.Response, error) {
+	data := struct {
 		CompanyID        string      `json:"cid"`
 		ProvisioningHash string      `json:"provhash"`
 		Command          string      `json:"cmd"`
-		Data             interface{} `json:"data"`
+		Payload             interface{} `json:"payload"`
 	}{
 		CompanyID:        c.CompanyID,
-		ProvisioningHash: c.Client.APIKey,
+		ProvisioningHash: c.APIKey,
 		Command:          command,
 	}
 
-	if data != nil {
-		body.Data = data
+	if payload != nil {
+		data.Payload = payload
 	}
 
-	return c.Client.RequestJSON(http.MethodPost, "", body)
+	// Form body.
+	body, err := lp.JSONReader(data)
+	if err != nil {
+		return nil, err
+	}
+
+	// Form Request.
+	req, err := http.NewRequest(http.MethodPost, c.URL.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add Header.
+	req.Header.Add("Content-Type", "application/json")
+	for header, values := range c.Headers {
+		for _, v := range values {
+			req.Header.Add(header, v)
+		}
+	}
+	req.Header.Set("X-Api-Key", c.APIKey)
+	req.Header.Set("User-Agent", c.UserAgent)
+
+	if c.Verbose {
+		dump, err := httputil.DumpRequest(req, true)
+		if err == nil {
+			log.Printf("%s", dump)
+		}
+	}
+
+	// Do Request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if c.Verbose {
+		dump, err := httputil.DumpResponse(resp, true)
+		if err == nil {
+			log.Printf("%s", dump)
+		}
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return resp, fmt.Errorf("API result failed: %s", resp.Status)
+	}
+
+	return resp, err
 }
